098894e8586ba7114a6f96fb0f9813fd02c7331f
yersonargote <llalex.2045@gmail.com>
2022-07-31 09:02:11 -0500

    Add: Print table as json

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from random import randrange
 from typing import Optional
 
@@ -11,13 +12,7 @@ def random(first: int = 0, last: int = 999) -> int:
 
 
 def show(table: dict):
-    print('--------------------------------')
-    for key, value in table.items():
-        print(f'{key}')
-        for i in value:
-            print('    ', end="")
-            print(i)
-    print('--------------------------------')
+    print(json.dumps(table, indent=4))
 
 
 def define_price(sales: dict) -> Optional[tuple]:
@@ -40,21 +35,55 @@ def define_q(amounts: dict) -> Optional[tuple]:
             return round(Q), rand
 
 
+def utility(price: float, amount: int):
+    return price * amount - 0.3 * amount - 0.15
+
+# print(f'Random: {rand1}')
+# print(f'Price: {price}')
+# print(f'Random: {rand2}')
+# print(f'Quantity: {q}')
+# print(f'Utility: {utlty}')
+
+
 def main():
     amounts = fill(AMOUNT)
     sales = fill(SALES)
 
+    simulatios: int = 25
+
+    random1: list = []
+    random2: list = []
+    prices: list = []
+    quantity: list = []
+    utilities: list = []
+
+    for _ in range(simulatios):
+        price, rand1 = define_price(sales)
+        q, rand2 = define_q(amounts)
+
+        utlty = utility(price, q)
+
+        random1.append(rand1)
+        prices.append(price)
+        random2.append(rand2)
+        quantity.append(q)
+        utilities.append(utlty)
+
     table: dict = {}
     sell_price: dict = {}
     amount_sould: dict = {}
+
+    sell_price["random"] = random1
+    sell_price["price"] = prices
+
+    amount_sould["random"] = random2
+    amount_sould["amount"] = quantity
+
     table["sell_price"] = sell_price
     table["amount_sould"] = amount_sould
+    table["utilities"] = utilities
 
-    price, rand1 = define_price(sales)
-    q, rand2 = define_q(amounts)
-
-    print(price, rand1)
-    print(q, rand2)
+    show(table)
 
 
 if __name__ == "__main__":
